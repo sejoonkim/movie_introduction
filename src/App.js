f1621aac1e501f064c3021d4098a7f8d717a1bae
sejoonkim <sejoondev@gmail.com>
2020-09-20 22:37:45 +0900

    feat: Add home, movie, movies components for react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Movies from "./components/Movies";
+import Movie from "./components/Movie";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/movies" component={Movies} />
+      <Route path="/movie/:id" component={Movie} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
