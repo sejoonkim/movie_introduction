330a064cdc877d5952310f4b2f3ea27c8fd34fb4
sejoonkim <sejoondev@gmail.com>
2020-09-20 22:47:22 +0900

    feat: Add navigation  component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import Movie from "./pages/Movie";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/movies" component={Movies} />
       <Route path="/movie/:id" component={Movie} />
