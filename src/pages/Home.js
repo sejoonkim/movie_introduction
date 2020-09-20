79c63bd5fff4094cc249d0cf82286e79847bc8de
sejoonkim <sejoondev@gmail.com>
2020-09-20 22:47:40 +0900

    feat: Add contents in home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>사용된 라이브러리 및 프레임 워크</h1>
+      <ul>
+        <li>@testing-library/jest-dom: ^4.2.4</li>
+        <li>@testing-library/react: ^9.3.2</li>
+        <li>testing-library/user-event: ^7.1.2</li>
+        <li>react: ^16.13.1</li>
+        <li>react-dom: ^16.13.1</li>
+        <li>react-router-dom: ^5.2.0</li>
+        <li>react-scripts: 3.4.3</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
