ac08e9e40409d4611fec9a576dee83ce7ef51d1c
sejoonkim <sejoondev@gmail.com>
2020-09-21 08:37:41 +0900

    style: Add responsive style component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import Movie from "./pages/Movie";
 import Navigation from "./components/Navigation";
+import "./App.css";
+import styled from "styled-components";
+
+const ResponsiveBlock = styled.div`
+  padding-left: 1rem;
+  padding-right: 1rem;
+  width: 1024px;
+  margin: 0 auto;
+
+  @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/movies" component={Movies} />
-      <Route path="/movie/:id" component={Movie} />
-    </BrowserRouter>
+    <ResponsiveBlock>
+      <BrowserRouter>
+        <Navigation />
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/movie/:id" component={Movie} />
+      </BrowserRouter>
+    </ResponsiveBlock>
   );
 }
 
